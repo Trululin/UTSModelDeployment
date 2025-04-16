78328d81fa7eda9a530c0bd4e92b5d6482afa8c3
Trululin <74391983+Trululin@users.noreply.github.com>
2025-04-16 17:01:49 +0700

    First Commit

---
+++ 2702208606_CarolineAng_hotelStreamlitApp.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import joblib 
+import numpy as np
+import pandas as pd
+
+model = joblib.load('outputModel.pkl')
+bookingStatusEncoder = joblib.load('bookingStatsEncode.pkl')
+mealPlanEncoder = joblib.load('mealPlanEncode.pkl')
+roomTypeEncoder = joblib.load('roomTypeEncode.pkl')
+marketSegmentEncoder = joblib.load('marketSegmentEncode.pkl')
+binaryEncoder = joblib.load('binaryEncode.pkl')
+
+def main():
+    st.title('Booking Status of A Hotel Customer')
+
+    Booking_ID = st.text_input('Your Booking ID')
+    no_of_adults = st.number_input('Number of Adults', min_value = 0, step=1)
+    no_of_children = st.number_input('Number of Children', min_value = 0, step=1)
+    no_of_weekend_nights = st.number_input('Number of Weekend Nights', min_value = 0, step=1)
+    no_of_week_nights = st.number_input('Number of Weekday Nights', min_value = 0, step=1)
+    type_of_meal_plan = st.radio('type_of_meal_plan', ['Not Selected','Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3'])
+    required_car_parking_space = st.radio('required_car_parking_space', ['Yes','No'])
+    room_type_reserved = st.radio('room_type_reserved', ['Room_Type 1','Room_Type 2', 'Room_Type 3', 'Room_Type 4', 'Room_Type 5', 'Room_Type 6', 'Room_Type 7'])
+    lead_time = st.number_input('Lead Time', min_value = 0, step=1)
+    arrival_year = st.number_input('Arrival Year', min_value = 2015, step=1)
+    arrival_month = st.number_input('Arrival Month', min_value = 1, max_value = 12, step=1)
+    arrival_date = st.number_input('Arrival Date', min_value = 1, max_value = 31, step=1)
+    market_segment_type = st.radio('market_segment_type', ['Online','Offline', 'Corporate', 'Complementary', 'Aviation'])
+    repeated_guest = st.radio('repeated_guest', ['Yes','No'])
+    no_of_previous_cancellations = st.number_input('Amount of Bookings Cancelled', min_value = 0, step=1)
+    no_of_previous_bookings_not_canceled = st.number_input('Amount of Bookings not Cancelled', min_value = 0, step=1)
+    avg_price_per_room = st.number_input('Average price per Room for each Night', min_value = 0.0)
+    no_of_special_requests = st.number_input('Number of Special Requests', min_value = 0, step=1)
+
+    data = { 
+        'no_of_adults' : int(no_of_adults), 
+        'no_of_children' : int(no_of_children), 
+        'no_of_weekend_nights' : int(no_of_weekend_nights), 
+        'no_of_week_nights' : int(no_of_week_nights),
+        'type_of_meal_plan' : type_of_meal_plan,
+        'required_car_parking_space' : required_car_parking_space,
+        'room_type_reserved' : room_type_reserved, 
+        'lead_time' : int(lead_time),
+        'arrival_year' : int(arrival_year), 
+        'arrival_month' : int(arrival_month),
+        'arrival_date'  : int(arrival_date), 
+        'market_segment_type' : market_segment_type,
+        'repeated_guest' : repeated_guest,
+        'no_of_previous_cancellations' : int(no_of_previous_cancellations), 
+        'no_of_previous_bookings_not_canceled' : int(no_of_previous_bookings_not_canceled), 
+        'avg_price_per_room' : float(avg_price_per_room),
+        'no_of_special_requests' : int(no_of_special_requests)
+    }
+
+    df = pd.DataFrame([list(data.values())], columns=['no_of_adults',
+                                                      'no_of_children', 
+                                                      'no_of_weekend_nights', 'no_of_week_nights',
+                                                      'type_of_meal_plan',
+                                                      'required_car_parking_space',
+                                                      'room_type_reserved', 
+                                                      'lead_time',
+                                                      'arrival_year',
+                                                      'arrival_month',
+                                                      'arrival_date', 
+                                                      'market_segment_type',
+                                                      'repeated_guest','no_of_previous_cancellations', 'no_of_previous_bookings_not_canceled', 'avg_price_per_room',
+                                                      'no_of_special_requests'])
+    
+    df = df.replace(binaryEncoder)
+
+    mealPlanEnc = df[['type_of_meal_plan']]
+    roomTypeEnc = df[['room_type_reserved']]
+    marketSegmentEnc = df[['market_segment_type']]
+
+    mealPlanEnc = pd.DataFrame(mealPlanEncoder.transform(mealPlanEnc).toarray(), columns=mealPlanEncoder.get_feature_names_out())
+    roomTypeEnc = pd.DataFrame(roomTypeEncoder.transform(roomTypeEnc).toarray(), columns=roomTypeEncoder.get_feature_names_out())
+    marketSegmentEnc = pd.DataFrame(marketSegmentEncoder.transform(marketSegmentEnc).toarray(), columns=marketSegmentEncoder.get_feature_names_out())
+
+    df = pd.concat([df, mealPlanEnc, roomTypeEnc, marketSegmentEnc], axis=1)
+    df = df.drop(columns = ['type_of_meal_plan', 'room_type_reserved', 'market_segment_type'], axis=1)
+
+    
+
+    if st.button('Get Booking Status'):
+        # st.dataframe(df)
+        inputData = df      
+        result = predictResult(inputData)
+        result = bookingStatusEncoder.inverse_transform(result)
+        st.success(f'Your Booking Status: {result[0]}')
+
+def predictResult(inputData):
+    inputArray = np.array(inputData).reshape(1, -1)
+    predictResult = model.predict(inputArray)
+    # st.dataframe(predictResult)
+    return predictResult
+
+if __name__ == '__main__':
+    main()
---
+++ 2702208606_CarolineAng_oopFormat.py
@@ -0,0 +1,171 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle as pkl
+import statistics
+
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from xgboost import XGBClassifier
+
+class DataMaker:
+    def __init__(self, filePath):
+        self.filePath = filePath
+        self.df = None
+        self.x = None
+        self.y = None
+    
+    def loadData(self):
+        self.df = pd.read_csv(self.filePath)
+
+    def createXY(self, targetColumn):
+        self.x = self.df.drop(targetColumn, axis=1)
+        self.y = self.df[targetColumn]
+
+    def removeUnUsedColumn(self, column):
+        self.x = self.x.drop(columns=[column], axis=1)
+
+
+class ModelMaker:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.createModel()
+        self.xTrain, self.xTest, self.yTrain, self.yTest, self.yCheck, self.yRfPred, self.yXgbPred, self.xTrainModel, self.xTestModel = [None]*9
+
+    def createModel(self, criteria='gini', objective='binary:logistic', randomState = 42):
+        self.rfModel = RandomForestClassifier(criterion=criteria, random_state=randomState)
+        self.xgbModel = XGBClassifier(objective=objective, random_state=randomState)
+    
+    def dataSplitting(self, testSize = 0.2, randomState = 42):
+        self.xTrain, self.xTest, self.yTrain, self.yTest = train_test_split(self.x, self.y, test_size=testSize, random_state=randomState)
+
+    def boxplotMaker(self, columns):
+        self.xTrain.boxplot(column=columns)
+        plt.show
+
+    def getMedianValue(self, column):
+        return np.median(self.xTrain[column].dropna())
+    
+    def getModeValue(self, column):
+        return statistics.mode(self.xTrain[column])
+    
+    def fillNaData(self, column, data):
+        self.xTrain[column] = self.xTrain[column].fillna(data)
+        self.xTest[column] = self.xTest[column].fillna(data)
+
+    def labelEncoderMaker(self):
+        bookingStatsEncoder = LabelEncoder()
+
+        self.yTrain = bookingStatsEncoder.fit_transform(self.yTrain)
+        self.yTest = bookingStatsEncoder.transform(self.yTest)
+        self.yCheck = bookingStatsEncoder.inverse_transform(self.yTest)
+
+        pkl.dump(bookingStatsEncoder, open('bookingStatsEncode.pkl', 'wb'))
+
+    def oneHotEncoderMaker(self, column, filename):
+        encoder = OneHotEncoder()
+
+        dataTrain = self.xTrain[[column]]
+        dataTest = self.xTest[[column]]
+
+        dataTrain = pd.DataFrame(encoder.fit_transform(dataTrain).toarray(), columns=encoder.get_feature_names_out())
+        dataTest = pd.DataFrame(encoder.transform(dataTest).toarray(), columns=encoder.get_feature_names_out())
+
+        pkl.dump(encoder, open(filename, 'wb'))
+
+        return dataTrain, dataTest
+
+    def binaryEncoderMaker(self, column):
+        binaryEncoder = {}
+        data = {'Yes' : 1, 'No' : 0}
+
+        for i in column:
+            binaryEncoder[i] = data
+
+        pkl.dump(binaryEncoder, open('binaryEncode.pkl', 'wb'))
+
+    
+    def combineColumns(self, train, test):
+        self.xTrain = self.xTrain.reset_index()
+        self.xTest = self.xTest.reset_index()
+
+        trainDf = pd.concat(train, axis=1)
+        testDf = pd.concat(test, axis=1)
+
+        self.xTrain = pd.concat([self.xTrain, trainDf], axis=1)
+        self.xTest = pd.concat([self.xTest, testDf], axis=1)
+
+    def removeEncodedColumn(self, column):
+        self.xTrainModel = self.xTrain.drop(columns = column, axis=1)
+        self.xTestModel = self.xTest.drop(columns = column, axis=1)
+
+    def trainModel(self):
+        self.rfModel.fit(self.xTrainModel, self.yTrain)
+        self.xgbModel.fit(self.xTrainModel, self.yTrain)
+
+    def makePrediction(self):
+        self.yRfPred = self.rfModel.predict(self.xTestModel)
+        self.yXgbPred = self.xgbModel.predict(self.xTestModel)
+        
+    def createReport(self):
+        reverseEncoder = pkl.load(open('bookingStatsEncode.pkl', 'rb'))
+        self.yRfPred = reverseEncoder.inverse_transform(self.yRfPred)
+        self.yXgbPred = reverseEncoder.inverse_transform(self.yXgbPred)
+
+        print("Classification Report for Random Forest")
+        print(classification_report(self.yCheck, self.yRfPred))
+
+        print("Classification Report for XGBoost")
+        print(classification_report(self.yCheck, self.yXgbPred))
+
+    def saveBestModel(self):
+        rfAcc = accuracy_score(self.yCheck, self.yRfPred)
+        xgbAcc = accuracy_score(self.yCheck, self.yXgbPred)
+
+        if (rfAcc > xgbAcc):
+            print("\nRandom Forest is the best model with Accuracy Score: %.3f" %rfAcc)
+            pkl.dump(self.rfModel, open('outputModel.pkl', 'wb'))
+        else:
+            print("\nXGB is the best model with Accuracy Score: %.3f" %xgbAcc)
+            pkl.dump(self.xgbModel, open('outputModel.pkl', 'wb'))
+        
+        
+
+filePath = 'Dataset_B_hotel.csv'
+dataMaker = DataMaker(filePath)
+dataMaker.loadData()
+dataMaker.createXY('booking_status')
+dataMaker.removeUnUsedColumn('Booking_ID')
+
+x = dataMaker.x
+y = dataMaker.y
+
+
+modelMaker = ModelMaker(x, y)
+modelMaker.dataSplitting()
+modelMaker.boxplotMaker(['required_car_parking_space', 'avg_price_per_room'])
+
+carParking = modelMaker.getMedianValue('required_car_parking_space')
+avgPrice = modelMaker.getMedianValue('avg_price_per_room')
+mealPlan = modelMaker.getModeValue('type_of_meal_plan')
+
+modelMaker.fillNaData('required_car_parking_space', carParking)
+modelMaker.fillNaData('avg_price_per_room', avgPrice)
+modelMaker.fillNaData('type_of_meal_plan', mealPlan)
+
+modelMaker.labelEncoderMaker()
+trainMeal, testMeal = modelMaker.oneHotEncoderMaker('type_of_meal_plan', 'mealPlanEncode.pkl')
+trainRoom, testRoom = modelMaker.oneHotEncoderMaker('room_type_reserved', 'roomTypeEncode.pkl')
+trainMarket, testMarket = modelMaker.oneHotEncoderMaker('market_segment_type', 'marketSegmentEncode.pkl')
+modelMaker.combineColumns(train=[trainMeal, trainRoom, trainMarket], test=[testMeal, testRoom, testMarket])
+modelMaker.binaryEncoderMaker(['required_car_parking_space', 'repeated_guest'])
+
+modelMaker.removeEncodedColumn(['index', 'type_of_meal_plan', 'room_type_reserved', 'market_segment_type'])
+
+modelMaker.trainModel()
+modelMaker.makePrediction()
+modelMaker.createReport()
+modelMaker.saveBestModel()
